4ddd102d367575dee1d1fe39d66cb5224c7894a1
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-02-28 21:39:33 -0500

    Update 217-contains-duplicate.py

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -3,14 +3,19 @@
 # TC = O(n^2)
 # SC = O(1) (we are not creating another array)
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 2
 # 2nd best approach- sort the array, so we will only compare neighbouring elements. Do this for all elements of array.
 # TC = O(nlogn) (n for all elements, log n for sorting)
 # SC = O(1)  (we are not creating another array)
 
-#3
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#3 Neetcode best solution
 # Best approach for time complexity- create a hashset. If an element is not present in the hashset, add it. If already present, return True
+# TC = O(n) (we are iterating only once)
+# SC = O(n) (we are creating a new set)
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
@@ -22,4 +27,19 @@ class Solution:
         seen.add(i)
      return False
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#4 My solution
+# Convert list to set, and compare length of list and set. Set will remove all duplicates. If the length of list != length of set, return True. 
+# TC = O(n) (converting list to set)
+# SC = O(n) (creating a new set)
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set(nums)
+        
+        if len(nums) != len(nums_set):
+            return True
+        return False
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
