76b378d9d3232c476d3f474778b2630e4cdaac43
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-02-28 21:24:28 -0500

    Create 217-contains-duplicate.py

---
+++ 217-contains-duplicate.py
@@ -0,0 +1,6 @@
+# <h1> 1 </h1> 
+# Brute Force- compare 1 item of array to all other items. Do this for all elements of array. Since we would be using for loop twice:
+# TC = O(n^2)
+# SC = O(1) as we are not creating another array
+
+
