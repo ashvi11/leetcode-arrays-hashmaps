4ea72da476d54fcaa2eca11a25b259d3375a2fe5
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-03-02 20:34:59 -0500

    Update 242-valid-anagram.py

--- 242-valid-anagram.py
+++ 242-valid-anagram.py
@@ -1,1 +1,24 @@
-#1
+#1 NEETCODE Best approach:
+# Anagram means two strings have same characters, same number of times, but they are jumbled. This means the length should be the same. So first, we compare lengths of
+# both the strings. IF they are not equal, they aren't Anagrams.
+# Next we create two hashmaps(dictionaries)
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+      if len(s) != len(t):
+        return False
+      
+      countS, countT = {}, {}
+      
+      for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+       for c in countS:
+        if countS[c] != countT.get(c, 0):
+          return False
+        
+       return True
+        
+       
