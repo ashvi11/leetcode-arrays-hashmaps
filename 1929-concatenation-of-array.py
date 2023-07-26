36d7e6b0158dcb67c59b0c4a7d2a003ed9ad2fc3
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-07-26 08:31:00 -0400

    Update 1929-concatenation-of-array.py

--- 1929-concatenation-of-array.py
+++ 1929-concatenation-of-array.py
@@ -1,4 +1,9 @@
 # NEETCODE Best Solution
+# Create a new array and push elements from nums- how many time? To answer that, we wrapped it in a for loop and kept range to 2.
+# If interviewer asks to do this 100 time, we just need to change range to 100- MOST EXTENSIBLE SOLUTION. es of elements(keys).
+
+# TC = O(n)~ 2n- pushing 1 element in the new array takes O(1) time, here we are pushing 2n elements
+# SC = O(n)~ 2n- we created a new array with length 2n, but since it was required in question, we can also think of it as O(1)
 
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
